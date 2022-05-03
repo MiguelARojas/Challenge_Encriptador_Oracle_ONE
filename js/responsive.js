942c8113106b04e0961361e4476c0ed60b854ebb
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-05-02 19:31:19 -0500

    Se genero los principales cambios en nuestra pantalla para que se visualice en un ipad(tablet)

--- js/responsive.js
+++ js/responsive.js
@@ -1,3 +1,14 @@
-if ((window.matchMedia("(min-width: 481px)").matches) && (window.matchMedia("(max-width: 929px)").matches)){
-    console.log("simon se cambio el tamaño ")
+// obtenemos el panel
+var panel = document.querySelector(".rectangle-panel");
+
+// obtenemos los botones
+var btnEncriptar = document.querySelector(".btn-encriptar");
+var btnDesencriptar = document.querySelector("btn-desencriptar");
+
+// validaremos que sea responsive para la tablet
+if ((window.matchMedia("(min-width: 768px)").matches) && (window.matchMedia("(max-width: 1024px)").matches)){
+    panel.style.height = "343px";
+}
+if(window.matchMedia("(max-width: 1024px)").matches){
+    panel.style.height = "595px";
 }
