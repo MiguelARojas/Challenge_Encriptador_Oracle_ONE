c4dea45a0d5f707471c10130c230efb81cd23cec
Miguel Rojas <miguelrojasmacias@gmail.com>
2022-05-02 20:30:12 -0500

    Se creo las medidas que abarcaria un telefono celular

--- js/responsive.js
+++ js/responsive.js
@@ -1,14 +1,22 @@
 // obtenemos el panel
 var panel = document.querySelector(".rectangle-panel");
 
-// obtenemos los botones
-var btnEncriptar = document.querySelector(".btn-encriptar");
-var btnDesencriptar = document.querySelector("btn-desencriptar");
-
 // validaremos que sea responsive para la tablet
 if ((window.matchMedia("(min-width: 768px)").matches) && (window.matchMedia("(max-width: 1024px)").matches)){
     panel.style.height = "343px";
 }
+
 if(window.matchMedia("(max-width: 1024px)").matches){
     panel.style.height = "595px";
+}
+
+
+
+// validaremos que sea responsive para un telefono
+if((window.matchMedia("(min-width: 320px)").matches) && (window.matchMedia("(max-width: 360px)").matches)){
+    panel.style.height = "133px";
+}
+
+if(window.matchMedia("(max-width: 360px)").matches){
+    panel.style.height = "186px";
 }
